bb5337e4afec4f78ab50474435889dc6e0d06139
Nikhilesh Chaudhary <nikhileshchaudhary@Nikhileshs-MacBook-Pro.local>
2019-08-11 14:51:55 -0700

    added changes

---
+++ my-app/src/Ninjas.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+
+class Ninjas extends Component {
+
+    render() {
+        return (
+            <div className = "Ninja"> 
+            <div> Name: Nikhil </div>
+            <div> Age: 40 </div>  
+            <div> Belt: Black </div>        
+            </div> 
+        )
+
+    }
+}
+export default Ninjas;
