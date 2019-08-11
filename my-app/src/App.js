9fb8ba5c9c170e1cb6f348570669a9c6d1db55e6
Nikhilesh Chaudhary <nikhileshchaudhary@Nikhileshs-MacBook-Pro.local>
2019-08-11 16:23:34 -0700

    changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
 import Ninjas from './Ninjas';
+import AddNinja from './AddNinja';
 
 class App extends Component {
 
@@ -15,6 +16,8 @@ class App extends Component {
     <div className="App">
     <h1> My first react app </h1>
     <Ninjas ninjas = {this.state.ninjas}/> 
+    <AddNinja/>
+    
     </div>
   );
 }
