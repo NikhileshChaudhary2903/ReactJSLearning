b779761574b099e7a03db2c8741d309dd1ecc659
Nikhilesh Chaudhary <nikhileshchaudhary@Nikhileshs-MacBook-Pro.local>
2019-08-13 22:54:35 -0700

    added

---
+++ todoapp/src/AddTodo.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+
+class AddTodo extends Component {
+
+    state = {
+        content : ''
+    }
+    
+    handleChange = (e) => {
+        this.setState({
+            content : e.target.value
+        });
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault(); // default action of form being submitted is page refreshed so prevent that
+        // console.log(this.state);
+        this.props.addTodo(this.state);
+        this.setState({ content : ''});
+    }
+render() {
+
+    return (
+        <div>
+            <form onSubmit = {this.handleSubmit}>
+            <label> Add New Todo Item : </label>
+            <input type = "text" onChange = {this.handleChange} value = {this.state.content}/>    
+            </form>    
+            
+        </div>
+
+    )
+
+}
+
+}
+
+export default AddTodo;
+
--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
-import Todos from './Todos'
+import Todos from './Todos';
+import AddTodo from './AddTodo';
+
 class App extends Component {
   
   state = {
@@ -14,9 +16,18 @@ class App extends Component {
   const todos = this.state.todos.filter(todo => {
     return todo.id !== id;
   });
-  this.setState ({todos : todos});
+  this.setState({todos : todos});
    
   }
+
+  addTodo = (todo) => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos,todo];
+    this.setState({
+      todos : todos
+    });
+  }
+
   render() {
   return (
     <div className="todo-app container">
@@ -24,6 +35,8 @@ class App extends Component {
       Todo's 
       </h1>
       <Todos todos = {this.state.todos} deleteTodo = {this.deleteTodo}/>
+      <AddTodo addTodo = {this.addTodo}/>
+
     </div>
   );
 }
