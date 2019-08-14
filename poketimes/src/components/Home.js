16ecaf9c6da2a6dda41d37e4a650cdfe8276f058
Nikhilesh Chaudhary <nikhileshchaudhary@Nikhileshs-MacBook-Pro.local>
2019-08-13 23:40:04 -0700

    new

---
+++ poketimes/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+
+    return (
+        <div className = "container"> 
+        <h4 className="center"> This is the Home page</h4>
+        <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia beatae quaerat libero modi amet iusto quisquam, molestias iste! Nobis veniam itaque tenetur voluptate commodi ipsa atque sapiente qui tempora porro.
+        </p></div>
+    )
+}
+export default Home;
+
