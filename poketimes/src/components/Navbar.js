0279fa1c32a394675e5e0df4698fb123d78fda18
Nikhilesh Chaudhary <nikhileshchaudhary@Nikhileshs-MacBook-Pro.local>
2019-08-13 23:53:56 -0700

    new

--- poketimes/src/components/Navbar.js
+++ poketimes/src/components/Navbar.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import {Link,NavLink} from 'react-router-dom';
+import {Link,NavLink,withRouter} from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = (props) => {
+
+    setTimeout( () => {
+        props.history.push('/about');
+    },2000);
 return (
     <nav className="nav-wrapper red darken-3">
         <div className="container">
@@ -18,5 +22,5 @@ return (
 
 }
 
-export default Navbar;
+export default withRouter(Navbar);
 
